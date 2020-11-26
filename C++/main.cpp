b6bfa52d174ced6d716c7e4addfe25d06227b101
shoury <shanuu12e@gmail.com>
2020-11-26 20:41:26 +0530

    Done more changes

--- C++/main.cpp
+++ C++/main.cpp
@@ -2,5 +2,27 @@
 
 int main(){
     std::cout << "Welcome to the C++ Calculator" << std::endl;
-    int num1,num2;
+    int num1,num2,choice;
+    const char *choices[4] = {"Addition","Substraction","Multiplication","Division"}; 
+    std::cout << "Enter the Number 1" << std::endl;
+    std::cin >> num1;
+    std::cout << "Enter the Number 2" << std::endl;
+    std::cin >> num2;
+    for(int i = 0;i<4;i++){
+        std::cout << choices[i] << std::endl;
+    }
+    std::cout << "Enter the choice" << std::endl;
+    std::cin >> choice;
+    if(choice == 1){
+        std::cout << "The sum is " << num1+num2 << std::endl;
+    }else if(choice == 2){
+        std::cout << "The difference is " << num1-num2 << std::endl;
+    }else if(choice == 3){
+        std::cout << "The product is " << num1*num2 << std::endl;
+    }else if(choice == 4){
+        std::cout << "The quotient is " << num1/num2 << std::endl;
+    }
+
+    return 0;
+
 }
--- C/main.c
+++ C/main.c
@@ -2,11 +2,29 @@
 
 int main(){
     printf("Welcome to C calculator");
-    int num1,num2;
+    int num1,num2,choice;
+    char *choices[4] = {"Addition","Substraction","Multiplication","Division"}; 
+
     printf("Enter the number 1");
     scanf("%d",&num1);
     printf("Enter the number 2");
     scanf("%d",&num2);
+
+    printf("Enter your choice");
+    for(int i = 0;i<4;i++){
+        printf(choices[i]);
+    }
+    scanf("%d",&choice);
+    if(choice == 1){
+        printf("The sum is %d", num1+num2);
+    }else if(choice == 2){
+        printf("The difference is %d", num1-num2);
+    }else if(choice == 3){
+        printf("The product is %d", num1*num2);
+    }else if(choice == 4){
+        printf("The quotient is %d", num1/num2);
+    }
+
 
 
 }
